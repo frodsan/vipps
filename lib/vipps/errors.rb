e408790c25c5a6f0d828e555137f9f76a9b5dbe1
Francesco Rodríguez <frodsan@me.com>
2022-03-07 22:46:38 +0100

    Minor refactor

--- lib/vipps/errors.rb
+++ lib/vipps/errors.rb
@@ -4,26 +4,26 @@ module Vipps
   class Error < StandardError
     class << self
       def from_response(response)
-        status = response[:status].to_i
+        klass = case response[:status].to_i
+          when 400 then BadRequest
+          when 401 then Unauthorized
+          when 403 then Forbidden
+          when 404 then NotFound
+          when 405 then MethodNotAllowed
+          when 406 then NotAcceptable
+          when 409 then Conflict
+          when 415 then UnsupportedMediaType
+          when 422 then UnprocessableEntity
+          when 451 then UnavailableForLegalReasons
+          when 400..499 then ClientError
+          when 500 then InternalServerError
+          when 501 then NotImplemented
+          when 502 then BadGateway
+          when 503 then ServiceUnavailable
+          when 500..599 then ServerError
+        end
 
-        case status
-        when 400 then BadRequest
-        when 401 then Unauthorized
-        when 403 then Forbidden
-        when 404 then NotFound
-        when 405 then MethodNotAllowed
-        when 406 then NotAcceptable
-        when 409 then Conflict
-        when 415 then UnsupportedMediaType
-        when 422 then UnprocessableEntity
-        when 451 then UnavailableForLegalReasons
-        when 400..499 then ClientError
-        when 500 then InternalServerError
-        when 501 then NotImplemented
-        when 502 then BadGateway
-        when 503 then ServiceUnavailable
-        when 500..599 then ServerError
-        end&.new(response)
+        klass&.new(response)
       end
     end
 
